7d3feec39a78f1ab24f0f3ba4e603c62875db479
3bbing <2be.ebbing@gmail.com>
2020-04-16 10:08:41 +0200

    Added Textarea include
    Forgot to include Textarea on top. Thanks @j0k3r

--- src/Wallabag/CoreBundle/Form/Type/EditEntryType.php
+++ src/Wallabag/CoreBundle/Form/Type/EditEntryType.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Wallabag\CoreBundle\Entity\Entry;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 
 class EditEntryType extends AbstractType
 {
